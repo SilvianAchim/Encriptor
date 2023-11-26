f018d67917870c20623cfd637db696b88d162069
SILVIANPC\silvi <silvianachim@hotmail.com>
2023-11-26 14:26:31 +0200

    Working

--- encriptor/src/services/files_handler.rs
+++
@@ -1,8 +0,0 @@
-use std::fs;
-
-// pub fn open_file(filepath: &str) -> String{
-//     let content = fs::read_to_string(filepath)
-//         .expect("Failed to read file!");
-//
-//     return content;
-// }
