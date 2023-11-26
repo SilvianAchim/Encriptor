5a10c3bfdc57d44abecf0b713bed14788d9e55fd
SILVIANPC\silvi <silvianachim@hotmail.com>
2023-11-26 10:10:33 +0200

    Version 1 of process

--- encriptor/src/services/files_handler.rs
+++ encriptor/src/services/files_handler.rs
@@ -1,8 +1,8 @@
 use std::fs;
 
-pub fn open_file(filepath: &str) -> String{
-    let content = fs::read_to_string(filepath)
-        .expect("Failed to read file!");
-
-    return content;
-}
+// pub fn open_file(filepath: &str) -> String{
+//     let content = fs::read_to_string(filepath)
+//         .expect("Failed to read file!");
+//
+//     return content;
+// }
