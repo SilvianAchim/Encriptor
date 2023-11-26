a4134ca525dcbeb72b4571905a4f1e3732bba0eb
SILVIANPC\silvi <silvianachim@hotmail.com>
2023-11-26 12:41:16 +0200

    Something working

--- encriptor/src/main.rs
+++ encriptor/src/main.rs
@@ -1,18 +1,20 @@
-use crate::services::logic::start;
+use std::env;
+
+use crate::services::logic::{decode, encode};
 use crate::startup::startup::read_config;
 
 mod models;
-mod startup;
 mod services;
+mod startup;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
     let config = read_config();
-    println!("Processes: {}", config.processes);
 
-    let file_path = "src/input.txt";
-
-    start(file_path, config.processes);
-
-    println!("Hello world1!");
+    match args.len() {
+        0 | 1 => panic!("Not enough arguments!"),
+        2 => encode(args[1].as_str(), config.processes),
+        3 => decode(args[1].as_str(), args[2].as_str(), config.processes),
+        _ => panic!("Too Many arguments!"),
+    }
 }
-
--- encriptor/src/startup/startup.rs
+++ encriptor/src/startup/startup.rs
@@ -1,10 +1,8 @@
-use std::fs;
 use crate::models::config::Config;
+use std::fs;
 
-pub fn read_config() -> Config{
-    let config_contents = fs::read_to_string("config.toml")
-        .expect("Failed to read config file");
+pub fn read_config() -> Config {
+    let config_contents = fs::read_to_string("config.toml").expect("Failed to read config file");
 
-    toml::from_str(&config_contents)
-        .expect("Failed to parse config")
+    toml::from_str(&config_contents).expect("Failed to parse config")
 }
